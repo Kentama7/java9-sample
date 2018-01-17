50a95395ac8c89c150bd3ad809c1e8b502846d4b
Kentama7 <Kentama7@users.noreply.github.com>
2018-01-17 23:01:27 +0900

    add interface and try-with-resource sample

--- Java9Sample/src/com/sample/etc/Sample.java
+++ Java9Sample/src/com/sample/etc/Sample.java
@@ -1,11 +1,13 @@
 package com.sample.etc;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // 匿名クラスでジェネリクスが不要
         List<String> list = new ArrayList<>() {
             @Override
@@ -16,5 +18,32 @@ public class Sample {
 
         // NG アンダースコア1つだけは使えなくなった
         // String _;
+
+        // try-with-resource文
+        FileInputStream is = new FileInputStream("foo");
+        try (is) {
+            // omit
+        } catch (Exception e) {
+        }
+
+        // Java8
+        try (FileInputStream is2 = new FileInputStream("foo")) {
+            // omit
+        } catch (Exception e) {
+        }
+    }
+
+    interface Foo {
+
+        private String bar() {
+            return "bar";
+        }
+
+        private String bar(String baz) {
+            return baz;
+        }
+
+        private static void qux() {
+        }
     }
 }
